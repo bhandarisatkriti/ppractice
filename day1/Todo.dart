ffb70427b0c881fe85b1c83b9a267a2609d300f2
bhandarisatkriti <satkriti32@gmail.com>
2024-01-07 18:21:23 +0545

    Basics

---
+++ day1/Todo.dart
@@ -0,0 +1,47 @@
+
+// import 'dart:html';
+
+// import 'package:flutter/material.dart';
+// void main()
+// {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget{
+//   MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home:Todo(),
+//     );
+//   }
+// }
+
+
+// class Todo extends StatefulWidgclset {
+//   const Todo({super.key});
+
+//   @override
+//   State<Todo> createState() => _TodoState();
+// }
+
+// class _TodoState extends State<Todo> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appbar:Appbar(
+//         leading:const Icon(Icons.add),
+//         title:Text("Todo"),
+//       ),
+//       body:Column(
+//         children:[
+//           floatingactionbutton:FloatingActionButton(
+//             child:Icon(Icons.add)
+//             onpressed:(){}
+//           )
+//         ]
+//       );
+//     )
+//   }
+// }
